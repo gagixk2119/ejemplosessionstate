7f12de4a57baeb6e3a67c88aac909bfd79219f7f
Gibran García <a385898@uach.mx>
2025-10-29 08:38:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ if 'count' not in st.session_state:
     st.session_state['count'] = 0
 
 if 'name' not in st.session_state:
-    st.session_state['name'] = 'gigiberto'
+    st.session_state['name'] 
     
 if st.button('Click me'):
     st.session_state['count'] += 1
 
 nombre = st.text_input("Escribe tu nombre")
 st.write(nombre)
+st.session_state.get("name", "")
 
 st.write(st.session_state)
