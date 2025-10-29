17ffe5487034b85bfb963f9af2ba512db4069601
Gibran García <a385898@uach.mx>
2025-10-29 07:46:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ st.title("Ejemplo para usar sesiion_state")
 
 count = 0
 
-increment = st.button('Increment')
+increment = st.button("Increment")
 if increment
     count += 1
 
-st.write('Count = ', count)
+st.write("Count = ", count)
